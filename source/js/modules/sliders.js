b51c507bbc7e2c92dc4faccfa1bccadf5d09cccc
Bebnev85 <andre44it@gmail.com>
2023-07-18 18:14:07 +0300

    Добавляет преимущества на страницу

--- source/js/modules/sliders.js
+++ source/js/modules/sliders.js
@@ -1,14 +1,15 @@
 export const sliderCoachInit = () => {
   const swiper = new Swiper('.coach-swiper__container', {
+    navigation: {
+      nextEl: '.coach-swiper__button-next',
+      prevEl: '.coach-swiper__button-prev',
+    },
+
     direction: 'horizontal',
     loop: false,
     slidesPerView: 1,
     slidesPerGroup: 1,
 
-    navigation: {
-      nextEl: '.coach-swiper__button-next',
-      prevEl: '.coach-swiper__button-prev',
-    },
     breakpoints: {
       768: {
         slidesPerView: 2,
