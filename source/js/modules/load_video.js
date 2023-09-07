f9f528b24f43a3b2bce81b151ef5410a7a0c6d8c
Bebnev85 <andre44it@gmail.com>
2023-09-07 13:29:09 +0300

    Исправляет ошибку у видео

--- source/js/modules/load_video.js
+++ source/js/modules/load_video.js
@@ -1,12 +1,13 @@
-
 export const initVideo = () => {
   const button = document.querySelector('.main-section__gim-video-conatainer');
   const video = document.getElementById('video');
   if (button) {
     button.addEventListener('click', () => {
-      const src = video.dataset.src;
-      video.style.height = '100%';
-      video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+      if (video) {
+        const src = video.dataset.src;
+        video.style.height = '100%';
+        video.insertAdjacentHTML('afterbegin', '<iframe src="' + src + '" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>');
+      }
     });
   }
 };
